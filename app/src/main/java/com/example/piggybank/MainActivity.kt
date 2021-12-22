5aeb4129f40a1bae23fc62d635ed9e674b20effe
griger <gustavo.rg92@gmail.com>
2021-12-22 12:45:34 +0100

    Create Android project that uses Jeckpack Compose :tada:

---
+++ app/src/main/java/com/example/piggybank/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.piggybank
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.piggybank.ui.theme.PiggybankTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PiggybankTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Text(text = "Piggybank")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PiggybankTheme {
+        Text(text = "Piggybank")
+    }
+}
